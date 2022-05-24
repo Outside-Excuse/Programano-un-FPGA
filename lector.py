db04f2313821c2f82a1cc790309009201f0b2ff2
Alejandro Hidalgo Badillo <alejandrohidalgobadillo@gmail.com>
2022-05-23 21:19:48 -0500

    programa AFD corregido

--- lector.py
+++ lector.py
@@ -1,142 +1,187 @@
-
-
-
-
+#Jorge Emiliano Turner Escalante 
+#Alejandro Hidalgo Badillo A01423412 
+from ast import expr
 from distutils.log import error
 from pickle import TRUE
 import re
-
+from tkinter import Variable
 
 def leerExpresiones(expresiones):
-    op = ['+', '-', '*', '/']
-    par = ['(', ')']
-    eq = '='
-    point = '.'
-    numbers = ['0','1','2','3','4','5','6','7','8','9','0']
-    abc = ['a','b','c','d','f','g','h','i','j','k','l','n','m','p','q','r','s','t','u','v','w','x','y','z']
-    dataBase = ['.','=','(', ')','+', '-', '*', '/','a','b','c','d','f','g','h','i','j','k','l','n','m','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','0']
-    #print(par[0], par[1])
-    print("lector de expresiones")
-    for i in expresiones:
-        conditional1 = False
-        conditional2 = False
-        description =[]
-        parentesis = 0
-        points = 0
-        exponentes = 0
-        variables = 0
-        variables2 = []
-        operacion = []
-        operadores = []
-        contOperadores = 0
-        comentario = 0
-        contadorEq = 0
-        comment = []
-        errorOp = False
-     
-        for elem in range (len(i)):
-            
-            operacion.append(i[elem])
-# confirmar uso de comentarios
-            if(i[elem]=='/'):
-                if (i[elem+1]=='/'):
-                    description.append("tienes un comentario")
-                    comentario = comentario + 1
+     database = ['a','b','c','d','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_']
+     numData = ['1','2','3','4','5','6','7','8','9','0']
+     operaciones = ['+','*','%','-']
+     punto = ['.']
+     parentesis = ['(',')']
+     exponente = ['e']
+     equal = ['=']
+     #print(expresiones)
+     for i in range(len(expresiones)):
+         description =[]
+         comentario = 0
+         contador = 0
+         type = []
+         numeros = []
+         operadores = []
+         puntos = []
+         puntoo = 0
+         parentesis2 = []
+         NUM =0
+         num_par = 0
+         Exponentes = []
+         contador_Exp = 0
+         igual = []
+         contador_igual = 0
+         for a in range(len(expresiones[i])):
+
+            if expresiones[i][contador] == "/":
+                if expresiones[i][contador+1] == "/":
+                    comentario = comentario+1
             if (comentario != 0):
-                comment.append(i[elem])
-            else:
-    #confirmar el uso de variables
-                for l in range (len(abc)):
-                    if (i[elem].lower()==abc[l]):
-                        variables = variables +1
-                        variables2.append(i[elem])
-    #confirmar exponente
-                if (i[elem]=='e'):
-                    exponentes = exponentes +1
-    # confirmar uso de operadores
-                for l in range (len(op)):
-                    if(i[elem]==op[l]):
-                        contOperadores = contOperadores + 1
-                        operadores.append(i[elem])    
-                        if(i[elem-1]==op[l]):
-                            print("este")
-                            errorOp = True 
-                        if((i[elem]==op[l] and i[elem-1]== '=')):
-                            errorOp = True
+                description.append(expresiones[i][contador])
+
+
+
+            #print(expresiones[i][contador])
+
+
+            #marcar variables 
+#variables---------------------->
+            contador2 = 0
+            typecount = []
+            contador3 = 0
+            numcount = []
+            contador4 = 0
+            operadorCount = []
+            contador5 = 0
+            puntoCount = []
+            contador6 = 0
+            parenCount = []
+            contador7 = 0
+            expCount = []
+            contador8 = 0
+            igualCount = []
+            if expresiones[i][contador].lower() != " " and comentario==0: 
+                for b in range(len(database)):
+                    #print(database[contador2]
+                    if database[contador2] == expresiones[i][contador].lower():
+
+                       
+                        typecount.append(expresiones[i][contador])
+                        contador2 = contador2+ 1
+                       
+
+
+
+                    else:
+                        contador2 = contador2+ 1
+
+                cad = ''.join(typecount)
+                type.append(cad)
+#Numeros--------------------->
+                for l in range(len(numData)):
+                    if numData[contador3] == expresiones[i][contador].lower():
+                        numcount.append(expresiones[i][contador])
+                        contador3 = contador3+1
+                        NUM = NUM +1
+                    else:
+                        contador3 = contador3 +1
+                cod = ''.join(numcount)
+                numeros.append(cod)
+#Operaciones------------------->
+                for j in range(len(operaciones)):
+                    if operaciones[contador4] == expresiones[i][contador].lower():
+                        operadorCount.append(expresiones[i][contador])
+                        contador4 = contador4+1
+                    else:
+                        contador4 = contador4 +1
+                ced = ''.join(operadorCount)
+                operadores.append(ced)
+
+                
+#pontus--------------------->
+                for k in range(len(punto)):
+                    if punto[contador5] == expresiones[i][contador].lower():
+                        puntoCount.append(expresiones[i][contador])
+                        contador5 = contador5+1
                         
+                        puntoo = puntoo +1
+                    else:
+                        contador5 = contador5+1
+                cid = ''.join(puntoCount)
+                puntos.append(cid)
+#parentesis-------------------->
+                for m in range (len(parentesis)):
+                    if parentesis[contador6] == expresiones[i][contador]:
+                        parenCount.append(expresiones[i][contador])
+                        contador6 = contador6+1
+                        num_par = num_par+1
+                    else:
+                        contador6 = contador6 + 1
+                cud =''.join(parenCount)
+                parentesis2.append(cud)
+#Exponentes------------------------>
+                for n in range (len(exponente)):
+                    if exponente[contador7] == expresiones[i][contador].lower():
+                        expCount.append(expresiones[i][contador])
+                        contador7 = contador7 +1
+                        contador_Exp = contador_Exp + 1
+                    else:
+                        contador7 = contador7+1
+                doom = ''.join(expCount)
+                Exponentes.append(doom)    
+#Asignacion------------------------------->  
+                for x in range (len(equal)):
+                    if equal[contador8] == expresiones[i][contador]:
+                        igualCount.append(expresiones[i][contador])    
+                        contador8 = contador8 + 1
+                        contador_igual = contador_igual +1
+                    else:
+                        contador8 = contador8 +1
+                kitten = ''.join(igualCount)
+                igual.append(kitten)      
+
+
+
                 
 
 
 
-    #confirmar uso de puntos
-                if (i[elem]==point):
-                    points = points + 1
-                    #print ("points: ", points)
 
-                #if points > 0:
-                #   for l in range (11):
-                #       if (i[elem-1] == numbers[l]):
-                #           conditional1 = True
-                #       if (i[elem+1] == numbers[l]):
-                #           conditional2 = True
-                #   if ((conditional2 == False) or (conditional1 == False)):
-                #       description.append("error, bad use of the '.' ")
-    # confirmar uso del operador igual
-                if (i[elem] == eq):
-                    contadorEq = contadorEq + 1
-                    if (i[elem+1] == par[0]) or (i[elem+1]==par[1]):
-                        for l in range (len(numbers)):
-                            if (i[elem+2].lower() == numbers[l] ):
-                                conditional1 = True 
-                            if (i[elem+2].lower() == abc[l] ):
-                                conditional2 = True 
-                    else:
-                        for l in range (len(numbers)):
-                            if (i[elem+1].lower() == numbers[l] ):
-                                conditional1 = True 
-                            if (i[elem+1].lower() == abc[l] ):
-                                conditional2 = True 
-    #confirmar parentesis
-                if (i[elem] == par[0]) or (i[elem]==par[1]):
-                    parentesis = parentesis + 1
-            
 
-#confirmar errores
 
-        if (contOperadores == 0):
-            description.append("no usa operadores")
-        if (errorOp == True):
-            description.append("mal uso de operadores")    
-        print(operacion)
-        if (conditional1 == True):
-            description.append("se iguala a un numero")
-        if (conditional2 == True):
-            description.append("se iguala a una variable")
-        print("contidad de operadores = ", contOperadores)
-        if(contOperadores > 0):
-            print("operadores usados : ", operadores)
-        print("comentarios = ",comentario)
-        if (comentario > 0):
-            print(comment)
-        print("exponentes = ", exponentes)
-        print("puntos = ",points)
-        print("parentesis = ",parentesis)
-        print("cantidad de variables = ",variables)
-        if(variables > 0 ):
-            print("variables usadas : ", variables2)
-        if parentesis > 0:
-            if (parentesis % 2 == 0):
-                description.append("right use of parenthesis")
-            else:
-                description.append("error, missing parenthesis")
 
-        if (points > 1):
-            description.append("error, so many '.' ")
-       
-        if (exponentes > 1):
-            description.append('error, mal uso de exponentes')
 
-        print(description,"<---------------")
+
+            contador = contador +1
+         
+
+#impresiones---------------------->
+         print("variables -----> ",type)
+         if contador_igual > 0:
+            print("Asignacion ----->",igual)
+         print("Operadores -----> ",operadores)
+         if NUM > 0:
+            print("numeros -----> ", numeros)
+        
+         if num_par > 0:
+             print("Parentesis -----> ", parentesis2)
+         if comentario > 0:
+            print("comentario -------> ", description)
+         if puntoo > 0:
+             print("puntos -----> ",puntos)
+         if contador_Exp > 0:
+             print("Exponente ----->", Exponentes)
+         print("--------------------fin de operacion--------------------")
+     
+
+
+
+                    
+
+
+
+           
+     
 
 # funcion main
 def main():
@@ -146,8 +191,8 @@ def main():
     expresiones = f.readlines()
     for i in range(len(expresiones)):
         expresiones[i] = expresiones[i].replace('\n', '')
-        expresiones[i] = expresiones[i].replace(' ', '')
+        
 
-    print(expresiones)
+   
     leerExpresiones(expresiones)
 main()
